f7f995472f43a19fd89c8ef0ade770c738763d46
Marc <mrivers2133@outlook.com>
2020-06-16 18:08:26 -0400

    Changed repo from scoring-margin to basic_rpg

---
+++ characters.py
@@ -0,0 +1,11 @@
+import equipment
+
+class Character:
+    def __init__(self, name, attack, defense):
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+
+    def equip(self):
+        # Equips weapons and armor according to user prompts
+        pass
---
+++ equipment.py
@@ -0,0 +1,11 @@
+class Weapon:
+    def __init__(self, name, dmg_type, atk_val):
+        self.name = name
+        self.dmg_type = dmg_type  # bludgeoning, piercing, slashing
+        self.atk_val = atk_val
+
+class Armor:
+    def __init__(self, name, arm_type, arm_val):
+        self.name = name
+        self.arm_type = arm_type  # light, medium, heavy
+        self.arm_val = arm_val
