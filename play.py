d6ea085226ab9a3aa665f453cca8bcd8f77acda9
Marc <mrivers2133@outlook.com>
2020-07-07 21:04:26 -0400

    Setting up basic play loop

--- play.py
+++ play.py
@@ -1,20 +1,34 @@
+import random
 # import characters (or just Character class?)
 
-if __name__ == '__main__':
+def change_equip():
+    # Allow player to change their equipment
     pass
 
-    def change_equip(self):
-        # Allow player to change their equipment
-        pass
-
-    def battle(self):
-        # Do battle with an enemy
-        # Damage based on each character's weapon damage value and armor value
-        pass
+def battle():
+    # Do battle with an enemy
+    # Damage based on each character's weapon damage value and armor value
+    pass
 
 
+if __name__ == '__main__':
     # Basic console menu
     # EQUIPMENT
     # FIGHT
     # EXIT
     # HELP
+
+    prompt = 'Welcome to this basic RPG! Please choose an option:'
+    prompt +=  '\n1. EQUIP - Change your character\'s equipment'
+    prompt +=  '\n2. FIGHT - Do battle with an enemy'
+    prompt +=  '\n3. EXIT - Exit the program\n\t'
+
+    menu = True
+    while menu == True:
+        message = input(prompt)
+
+        if message.upper() == 'EXIT' or message == '3':
+            print('Thank you for playing. Good bye!')
+            menu = False
+        else:
+            print('The loop works so far')
