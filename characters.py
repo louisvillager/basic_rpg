5ed537cc6fdded7539b1e8b095dc2465a8bace7b
Marc <mrivers2133@outlook.com>
2020-07-10 21:59:14 -0400

    Moved equipment functionality to characters.py

--- characters.py
+++ characters.py
@@ -1,10 +1,12 @@
-import equipment
+import csv
 
 class Character:
     def __init__(self, name, attack, defense):
         self.name = name
         self.attack = attack
         self.defense = defense
+        self.weapon = ''
+        self.armor = ''
         self.hp = 50
 
     def equip(self):
