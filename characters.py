a2c25971c3c6f12f86e0a6c25a0338c073aec262
Marc <mrivers2133@outlook.com>
2020-07-12 21:11:36 -0400

    Finished equipment change functionality

--- characters.py
+++ characters.py
@@ -15,22 +15,24 @@ class Character:
         # Equips weapon according to user prompts
         weap_type = input('What weapon type? (Slash, Pierce, Blunt?)  ')
 
-        with open('weapons.csv', 'r') as weapon_csv:
-            weap_reader = csv.reader(weapon_csv)
+        with open('weapons.csv', 'r') as f:
+            weap_reader = csv.reader(f)
 
             for line in weap_reader:
                 if weap_type == line[1]:
                     self.weapon = line[0]
                     self.attack = self.base_attack + int(line[2])
+                    print(f'{self.name} equipped a {self.weapon}.')
 
     def equip_armor(self):
         # Equips armor according to user prompts
         arm_type = input('What armor type? (Mage, Light, or Heavy)  ')
 
-        with open('armor.csv', 'r') as armor_csv:
-            arm_reader = csv.reader(armor_csv)
+        with open('armor.csv', 'r') as f:
+            arm_reader = csv.reader(f)
 
             for line in arm_reader:
                 if arm_type == line[1]:
                     self.armor = line[0]
                     self.defense = self.base_defense + int(line[2])
+                    print(f'{self.name} equipped {self.armor} armor.')
--- play.py
+++ play.py
@@ -1,9 +1,20 @@
 import random
-# import characters (or just Character class?)
+
+from characters import Character
+
 
 def change_equip():
     # Allow player to change their equipment
-    pass
+    equip_option = input('\nWhich equipment would you like to change?'
+        '\n1. Weapon'
+        '\n2. Armor'
+        '\n3. None\n\t')
+    if equip_option.lower() == 'weapon' or equip_option == '1':
+        hero.equip_weapon()
+    elif equip_option.lower() == 'armor' or equip_option == '2':
+        hero.equip_armor()
+    else:
+        pass
 
 def battle():
     # Do battle with an enemy
@@ -12,14 +23,16 @@ def battle():
 
 
 if __name__ == '__main__':
-    # Basic console menu
+    # Main menu
     # EQUIPMENT
     # FIGHT
     # EXIT
-    # HELP
 
-    prompt = 'Welcome to this basic RPG! Please choose an option:'
-    prompt +=  '\n1. EQUIP - Change your character\'s equipment'
+    name_hero = input('Welcome to this basic RPG! Please name your character.  ')
+    hero = Character(name_hero)
+    print(f'\nAre you ready for adventure, {hero.name}? How will you proceed?')
+
+    prompt =  '\n1. EQUIP - Change your character\'s equipment'
     prompt +=  '\n2. FIGHT - Do battle with an enemy'
     prompt +=  '\n3. EXIT - Exit the program\n\t'
 
@@ -27,8 +40,12 @@ if __name__ == '__main__':
     while menu == True:
         message = input(prompt)
 
-        if message.upper() == 'EXIT' or message == '3':
-            print('Thank you for playing. Good bye!')
+        if message.upper() == 'EQUIP' or message == '1':
+            change_equip()
+        elif message.upper() == 'FIGHT' or message == '2':
+            pass
+        elif message.upper() == 'EXIT' or message == '3':
+            print('Thank you for playing. Good bye!\n')
             menu = False
         else:
-            print('The loop works so far')
+            print('Please choose one of the available options.')
